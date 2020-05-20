41f64597adde520a6c856ed0e1ce433bd8640936
Luiz <lippe.ferreira@yahoo.fr>
2020-05-20 16:34:58 +0200

    Reglages des parametres d'affichage

--- app/src/main/java/com/example/programmation_mobile_projet/data/BeerApi.java
+++ app/src/main/java/com/example/programmation_mobile_projet/data/BeerApi.java
@@ -8,6 +8,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface BeerApi {
-    @GET("/v2/beers")
+    @GET("/v2/beers?page=1&per_page=80")
     Call<List<Beer>> getBeerResponse();
 }
